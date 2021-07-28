e9ba5f833106962a098ec47eb6a541da0d2e6b86
rslewenstein <rafasoaresjf@gmail.com>
2021-07-27 22:47:11 -0300

    Implementing MessageReceiver

---
+++ app/RabbitMQConsumer/MessageReceiver.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQConsumer
+{
+    public class MessageReceiver : DefaultBasicConsumer
+    {
+        private readonly IModel _channel;
+        public MessageReceiver(IModel channel)
+        {
+            _channel = channel;
+        }
+
+        public override void HandlerBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered,
+                                                 string exchange, string routingKey,
+                                                 IBasicProperties properties, byte[] body)
+        {
+            Console.WriteLine($"Consuming Topic Message");
+            Console.WriteLine(string.Concat("Message received from the exchange ", exchange));
+            Console.WriteLine(string.Concat("Consumer tag: ", consumerTag));
+            Console.WriteLine(string.Concat("Delivery tag: ", deliveryTag));
+            Console.WriteLine(string.Concat("Routing tag: ", routingKey));
+            Console.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(body)));
+            _channel.BasicAck(deliveryTag, false);
+        }
+    }
+}
